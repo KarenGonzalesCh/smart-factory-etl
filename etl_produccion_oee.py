70c6739a5522c15edbeaf5f0d5a8bababa77e6b8
Karen Gonzales <167720700+KarenGonzalesCh@users.noreply.github.com>
2026-07-27 22:13:52 -0300

    Update etl_produccion_oee.py

--- etl_produccion_oee.py
+++ etl_produccion_oee.py
@@ -23,6 +23,9 @@ else:
   creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
 
 client = gspread.authorize(creds)
+gc = client
+
+
 # 2. Enlaces oficiales de las Hojas de Google Sheets
 url_app_iny = 'https://docs.google.com/spreadsheets/d/1k0KyYQzkPanwmkI7Sx4GUHwdzR9IeXcduUudGCqv8Ks/edit?gid=80104312#gid=80104312'
 url_ren_disp = 'https://docs.google.com/spreadsheets/d/1MJ28yOstYa2XMzEuuI96F7ezi5myjTzxrb1fYAGPiZg/edit?gid=0#gid=0'
